2c8a8ba92d1fbccdcff8a7e5c70dd35219345b0c
Andy Lochan <36180382+Andylochan@users.noreply.github.com>
2021-06-09 15:50:57 -0400

    Add SDWebImage

---
+++ Fetch/Keys.swift
@@ -0,0 +1,11 @@
+//
+//  Keys.swift
+//  Fetch
+//
+//  Created by Andy Lochan on 6/9/21.
+//
+
+import SwiftUI
+
+let clientID = "MjIxNzM2OTR8MTYyMzI2NzM3Ny4xMDcyODY1"
+let clientSecret = "99b0cc74a6fe2db72246ad86a9cc19044c384098a1d240ec971c6dc8d245fb45"
