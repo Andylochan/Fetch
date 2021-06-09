74d64f05a95df7edbd42ef9c9f7852a3c7522c1c
Andy Lochan <36180382+Andylochan@users.noreply.github.com>
2021-06-09 15:42:42 -0400

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Andy Lochan on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
